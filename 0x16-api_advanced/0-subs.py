85e0b774893e5d19062377d4623901358e69b699
Mohamed Ilyess Issaoui <issaouimohamedilyess@gmail.com>
2024-03-18 14:19:58 +0100

    fix some requirements

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a giver subreddit"""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
@@ -12,5 +12,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get('data')
-    return results.get('subscribers')
+    results = response.json().get("data")
+    return results.get("subscribers")
